6e8bc5a746743a5d9b76bf97ac8ae36b3720624d
Shubham Kumbhar <88823169+Shubham-Kumbhar@users.noreply.github.com>
2024-04-13 13:22:26 +0530

    update: UI Mimplimented FPS Issure Resolved

--- Assets/Scripts/Experiment/Experiments.cs
+++ Assets/Scripts/Experiment/Experiments.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Experiments : MonoBehaviour
 {
     [SerializeField] private GameObject[] gameObjects;
     [SerializeField] private ExperimentEvents[] experimentEvent;
+    [SerializeField] private Experiment nextScene;
+
     private void Start()
     {
         experimentEvent = LevelManager.Instance.ExperimentEvents;
@@ -22,7 +25,18 @@ public class Experiments : MonoBehaviour
         foreach (GameObject objects in gameObjects)
         {
             objects.SetActive(false);
+            Invoke(nameof(ChnageScene), 2f);
         }
     }
+    public void ChnageScene()
+    {
+        SceneManager.LoadScene((int)nextScene);
+    }
 
+}
+public enum Experiment
+{
+    MainMenu,
+    TutRoom0,
+    PhysicRoom1
 }
